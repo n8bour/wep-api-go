e360be6ca63eac60b25bf502bd81dd23cc7237e2
Cláduio Almeida <cmma89@gmail.com>
2023-04-20 09:24:07 +0100

    some improvements on the code

--- internal/test-provider.go
+++ internal/test-provider.go
@@ -5,11 +5,12 @@ import (
 	"gorm.io/gorm"
 )
 
-type TestFetcher interface {
-	GetTest() (models.Test, error)
+type TestDAO interface {
+	GetTest() ([]models.Test, error)
+	CreateTest(test models.Test) (models.Test, error)
 }
 
-func NewTestFetcher(db *gorm.DB) TestRepository {
+func NewTestRepository(db *gorm.DB) TestRepository {
 	return TestRepository{DB: db}
 }
 
@@ -17,14 +18,13 @@ type TestRepository struct {
 	DB *gorm.DB
 }
 
-func (t *TestRepository) GetTest() ([]models.Test, error) {
-	var dest []models.Test
+func (t TestRepository) GetTest() (dest []models.Test, err error) {
 	t.DB.Model(&models.Test{}).Find(&dest)
 
 	return dest, nil
 }
 
-func (t *TestRepository) CreateTest(test models.Test) (models.Test, error) {
+func (t TestRepository) CreateTest(test models.Test) (models.Test, error) {
 	t.DB.Create(&test)
 
 	return test, nil
--- pkg/test-service.go
+++ pkg/test-service.go
@@ -13,11 +13,11 @@ import (
 )
 
 type TestService struct {
-	Fetcher internal.TestRepository
+	TestRepository internal.TestRepository
 }
 
 func NewServiceTest(db *gorm.DB) TestService {
-	return TestService{Fetcher: internal.NewTestFetcher(db)}
+	return TestService{TestRepository: internal.NewTestRepository(db)}
 }
 func (t TestService) Routes() chi.Router {
 	r := chi.NewRouter()
@@ -30,7 +30,7 @@ func (t TestService) Routes() chi.Router {
 func (t TestService) GetTest(writer http.ResponseWriter, _ *http.Request) {
 	writer.Header().Set(renderer.ContentType, renderer.ContentJSON)
 
-	test, err := t.Fetcher.GetTest()
+	test, err := t.TestRepository.GetTest()
 	if err != nil {
 		log.Fatal(" error fetching test")
 	}
@@ -52,7 +52,7 @@ func (t TestService) PostTest(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println(rs.Name)
 
-	test, err := t.Fetcher.CreateTest(rs)
+	test, err := t.TestRepository.CreateTest(rs)
 	if err != nil {
 		log.Fatal(err)
 	}
